c1e5b4db53a9b80df36582378dcd5b68f77458f4
MateusMarquessz <mateusmarques53@outlook.com>
2023-11-11 03:06:11 -0300

    att

--- Site/assets/script.js
+++ Site/assets/script.js
@@ -37,3 +37,13 @@ for (var i = 0; i < fundo.length; i++) {
       });
     });
   });
+
+        document.addEventListener("DOMContentLoaded", function() {
+            var arrow = document.querySelector('.work h2::after');
+            if (arrow) {
+                arrow.style.position = 'relative';
+                arrow.style.top = '0';
+            } else {
+                console.error('Elemento não encontrado.');
+            }
+        });
