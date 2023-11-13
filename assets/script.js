8d97a3ffc56f52d7eb2296079522c0cfefcf69a8
MateusMarquessz <mateusmarques53@outlook.com>
2023-11-12 23:06:01 -0300

    att

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,48 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var fundos = document.querySelectorAll(".fundo");
+document.addEventListener("DOMContentLoaded", function () {
+  let fundos = document.querySelectorAll(".fundo");
 
-  fundos.forEach(function(fundo) {
-      setTimeout(function() {
-          fundo.style.opacity = "1";
+  fundos.forEach(function (fundo, index) {
+    setTimeout(function () {
+      fundo.style.opacity = "1";
 
-          fundo.classList.add(Math.random() > 0.5 ? "movimento-direita" : "movimento-esquerda");
-      }, Math.random() * 2000);
+      fundo.classList.add(index % 2 === 0 ? "movimento-direita" : "movimento-esquerda");
+    }, Math.random() * 2000);
   });
+
+  const fundoText = ["HI, I'M MATEUS", "HI, I'M MATEUS", "HI, I'M MATEUS", "HI, I'M MATEUS", "HI, I'M MATEUS", "HI, I'M MATEUS", "HI, I'M MATEUS", "HI, I'M MATEUS", "HI, I'M MATEUS", "HI, I'M MATEUS", "I'M MATEUS"];
+  const body = document.body;
+
+  for (let i = 0; i < fundoText.length; i++) {
+    const paragrafo = document.createElement("p");
+    paragrafo.classList.add("fundo");
+
+    if (i % 2 === 0) {
+      paragrafo.classList.add("left");
+    }
+
+    const partes = fundoText[i].split(" ");
+
+    partes.forEach((parte, index) => {
+      const span = document.createElement("span");
+      span.textContent = parte;
+
+      if (parte === "MATEUS") {
+        span.classList.add("nome-mateus");
+      }
+
+      paragrafo.appendChild(span);
+
+      if (index < partes.length - 1) {
+        paragrafo.appendChild(document.createTextNode(" "));
+      }
+    });
+
+    body.appendChild(paragrafo);
+  }
 });
 
-var fundo = ["HI, I'M MATEUS", "HI, I'M MATEUS", "HI, I'M MATEUS","HI, I'M MATEUS", "HI, I'M MATEUS", "HI, I'M MATEUS","HI, I'M MATEUS", "HI, I'M MATEUS", "HI, I'M MATEUS","HI, I'M MATEUS", "HI, I'M MATEUS"];
 
-var body = document.body;
-
-for (var i = 0; i < fundo.length; i++) {
-  var paragrafo = document.createElement("p");
-  paragrafo.classList.add("fundo");
-
-  // Adiciona a classe "left" para metade das frases
-  if (i % 2 === 0) {
-    paragrafo.classList.add("left");
-  }
-
-  paragrafo.textContent = fundo[i];
-  body.appendChild(paragrafo);
-}
-
+//Click
   document.addEventListener('DOMContentLoaded', function() {
     var workDiv = document.querySelector('.work');
 
