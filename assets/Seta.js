37b657240cb251444b4df709f70f59b568f93598
MateusMarquessz <mateusmarques53@outlook.com>
2024-01-28 21:51:41 -0300

    Atualizacao das ferias
    Atualizacao nas ferias da faculdade

--- assets/Seta.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var arrow = document.querySelector('.work h2::after');
-      if (arrow) {
-          arrow.style.position = 'relative';
-          arrow.style.top = '0';
-        } else {
-            console.error('Elemento não encontrado.');
-          }
-      });
