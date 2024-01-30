d0d695c92a8c8c4055ae1ad863c938179304c6f3
MateusMarquessz <mateusmarques53@outlook.com>
2024-01-30 20:26:43 -0300

    Corrigindo erro js

--- assets/AniRolagem.js
+++ assets/AniRolagem.js
@@ -2,8 +2,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        const targetId = this.getAttribute('href');
+        if (targetId) {
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        }
     });
 });
